021a1c978feaeff5579cb1c78ca5770e9c2501d2
apple <apple@Student.local>
2017-08-25 13:18:54 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/t00385/android2017a/guard/SplashActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.t00385.android2017a.guard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
