6de7774bc401575cfd8a40bd57f867ecf621963f
cuiyu <cuiyu@gdmec.edu.cn>
2017-09-03 15:16:50 +0800

    ui automator sample is ok

--- app/src/androidTest/java/cn/edu/gdmec/t00385/android2017a/guard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/t00385/android2017a/guard/ExampleInstrumentedTest.java
@@ -1,12 +1,23 @@
 package cn.edu.gdmec.t00385.android2017a.guard;
 
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +26,50 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "cn.edu.gdmec.t00385.android2017a.guard";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
-        assertEquals("cn.edu.gdmec.t00385.android2017a.guard", appContext.getPackageName());
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void testVersion() throws Exception {
+        UiObject2 object2 ;
+        object2 = mDevice.findObject(By.clazz("android.widget.Button"));
+        assertEquals("按钮查看","LOGIN",object2.getText());
+    }
+    @Test
+    public void testVersion1() throws Exception {
+        UiObject result = mDevice.findObject(new UiSelector().textStartsWith("ver"));
+        String resultText = result.getText();
+        assertEquals("版本检查结果1", "version:1.0", resultText);
     }
 }
